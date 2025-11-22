c6dde532b9dff1c6998e9cdf78b96409f7c5b27e
skabadze <148717744+skabadze@users.noreply.github.com>
2025-11-22 12:01:49 +0000

    updated install script

--- install.sh
+++ install.sh
@@ -5,10 +5,22 @@ if [ "$EUID" -eq 0 ]
   exit
 fi
 
-sudo pacman --noconfirm -S fastfetch fuzzel kitty swaync waybar wlogout hyprland hyprlock hypridle hyprpolkitagent swww ufw git base-devel
+sudo pacman --noconfirm -S fastfetch fuzzel kitty thunar tumbler swaync waybar hyprland hyprlock hypridle hyprpolkitagent nwg-look pipewire pavucontrol bluez blueman mpv ttf-roboto ttf-nerd-fonts-symbols otf-font-awesome swww btop firewalld flatpak git base-devel
 
-sudo enable ufw 
-sudo systemctl enable ufw
+sudo systemctl enable firewalld
+sudo systemctl start firewalld
+
+swww-daemon & disown
+swww img ./gruvbox_girl.png 
+
+cd ~
+mkdir git-clones
+cd git-clones
+git clone https://aur.archlinux.org/paru-bin.git
+cd paru-bin
+makepkg -si
+
+paru --noconfirm -S wlogout waypaper
 
 cd ./.config/
 mkdir ~/.config
@@ -17,14 +29,9 @@ cp -r * ~/.config
 hyprctl reload
 killall waybar
 waybar & disown
-swww-daemon & disown
-swww img ./gruvbox_girl.png 
 
-cd ~
-mkdir git-clones
-cd git-clones
-git clone https://github.com/Morganamilo/paru-bin.git
-cd paru-bin
-makepkg -si
+sudo pacman -S blender krita kdenlive wine steam heroic-games-launcher qbittorrent supertuxcart audacious bitwarden openrgb element-desktop prism-launcher libreoffice code thunderbird
+
+paru -S vesktop mullvad-vpn-bin librewolf-bin 
 
 echo "You should restart your PC now."
